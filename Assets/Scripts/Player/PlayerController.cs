56170627aba5ecf507880ccb9c1af821e7890b02
Musteba Erkan GUNAY <merkang91@gmail.com>
2022-05-19 13:14:32 +0300

    Commit
    Small changes on input system.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,76 +5,25 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private Character characterControls;
-    private Rigidbody2D rb;
+    private Character character;
 
-    private float inputX;
-
-    [SerializeField] private float moveSpeed, jumpForce;
-
-    public Transform groundPoint;
-    public LayerMask whatIsGround;
-
-
-    private bool isGrounded = true;
-
-    public Animator playerAnim;
-
+    private void Awake()
+    {
+        character = new Character();
+    }
 
     private void OnEnable()
     {
-        characterControls.Enable();
+        character.Enable();
     }
 
     private void OnDisable()
     {
-        characterControls.Disable();
-    }
-
-    private void Awake()
-    {
-        characterControls = new Character();
-    }
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
+        character.Disable();
     }
 
     private void Update()
     {
-
-        rb.velocity = new Vector2(inputX * moveSpeed, rb.velocity.y);
-
-        isGrounded = Physics2D.OverlapCircle(groundPoint.position, .2f, whatIsGround);
-
-        if(rb.velocity.x > 0f)
-        {
-            transform.localScale = Vector3.one;
-            playerAnim.SetBool("isRunning", true);
-        }
-        else if(rb.velocity.x < 0f)
-        {
-            transform.localScale = new Vector3(-1f, 1f, 1f);
-            playerAnim.SetBool("isRunning", true);
-        }
-        else if (rb.velocity.x == 0f)
-        {
-            playerAnim.SetBool("isRunning", false);
-        }
-        
-    }
-
-    public void Move(InputAction.CallbackContext context)
-    {
-        inputX = context.ReadValue<Vector2>().x;
-    }
-
-    public void Jump(InputAction.CallbackContext context)
-    {
-        if (isGrounded)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        }
+        float movementInput = character.Player.Movement.ReadValue<float>();
     }
 }
