3cf1e488cb3c4ba2d1919c9790e982c5e65f2002
Musteba Erkan GUNAY <merkang91@gmail.com>
2022-07-09 23:14:28 +0700

    PlayerAttackState
    -Animations are added.
    -Transitions are added.

--- Assets/Scripts/Player/PlayerStates/SubStates/PlayerAttackState.cs
+++ Assets/Scripts/Player/PlayerStates/SubStates/PlayerAttackState.cs
@@ -4,7 +4,28 @@ using UnityEngine;
 
 public class PlayerAttackState : PlayerAbilityState
 {
+    private Weapon weapon;
     public PlayerAttackState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName) : base(player, stateMachine, playerData, animBoolName)
     {
+
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+
+        weapon.EnterWeapon();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+
+        weapon.ExitWeapon();
+    }
+
+    public void SetWeapon(Weapon weapon)
+    {
+        this.weapon = weapon;
     }
 }
--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -4,5 +4,32 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    private Animator baseAnimator;
+    private Animator weaponAnimator;
 
+    protected virtual void Start()
+    {
+        baseAnimator = transform.Find("Base").GetComponent<Animator>();
+        weaponAnimator = transform.Find("Weapon").GetComponent<Animator>();
+
+        gameObject.SetActive(false);
+    }
+
+    public virtual void EnterWeapon()
+    {
+        gameObject.SetActive(true);
+
+        baseAnimator.SetBool("attack", true);
+        weaponAnimator.SetBool("attack", true);
+    }
+
+    public virtual void ExitWeapon()
+    {
+        gameObject.SetActive(false);
+
+        baseAnimator.SetBool("attack", false);
+        weaponAnimator.SetBool("attack", false);
+    }
 }
+
+
