094e841fe560aae58c98878673145fd83ed39b88
Musteba Erkan GUNAY <merkang91@gmail.com>
2022-07-11 02:51:30 +0700

    Weapon Inventory

--- Assets/Scripts/Player/PlayerFSM/Player.cs
+++ Assets/Scripts/Player/PlayerFSM/Player.cs
@@ -32,6 +32,7 @@ public class Player : MonoBehaviour
     public Rigidbody2D RB { get; private set; }
     public Transform DashDirectionIndicator { get; private set; }
     public BoxCollider2D MovementCollider { get; private set; }
+    public PlayerInventory Inventory { get; private set; }
     #endregion
 
     #region Check Transforms
@@ -77,8 +78,12 @@ public class Player : MonoBehaviour
         RB = GetComponent<Rigidbody2D>();
         DashDirectionIndicator = transform.Find("DashDirectionIndicator");
         MovementCollider = GetComponent<BoxCollider2D>();
+        Inventory = GetComponent<PlayerInventory>();
 
         FacingDir = 1;
+
+        PrimaryAttackState.SetWeapon(Inventory.weapons[(int)CombatInputs.primary]);
+        SecondaryAttackState.SetWeapon(Inventory.weapons[(int)CombatInputs.primary]);
 
         StateMachine.Initialize(IdleState);
     }
@@ -171,6 +176,7 @@ public class Player : MonoBehaviour
 
 
     #endregion
+
     #region Other Functions
 
     public void SetColliderHeight(float height)
